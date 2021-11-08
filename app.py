62bf00294fa736f3bf3b8da11d3164c14c118fac
YeJun <hayejun1013@naver.com>
2021-11-09 03:06:57 +0900

    feat: add flask-migrate module and migrate files
    - 플라스크 마이그레이션 모듈 추가
    - 마이그레이트 파일 생성

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_migrate import Migrate
+
 from company.models import db
+from config import DB_URL
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI']        = "mysql+pymysql://root:1234@localhost:3306/wantedlab"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']  = True
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
 
 db.init_app(app)
 db.app = app
 db.create_all()
+migrate = Migrate(app, db)
 
-@app.route("/ping", methods = ['GET'])
+
+@app.route("/ping", methods=["GET"])
 def ping():
     return "pong"
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
