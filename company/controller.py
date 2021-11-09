7645454e29ce0aa0ed2d02bb56b42e6d1bdf42fe
Chi heon Song <hrpp1353@gmail.com>
2021-11-09 13:13:23 +0900

    feat: Create Get Company Detail Info and Pass the test case
    - relationship 오류 해결
    - 회사 상세 정보 검색 기능 구현
    - 테스트 케이스 성공(status 200 & 404)

--- company/controller.py
+++ company/controller.py
@@ -1,3 +1,4 @@
+from flask         import jsonify, make_response
 from flask_restful import Resource, request
 
 from company.models import *
@@ -27,3 +28,37 @@ class CompanyList(Resource):
             return {"message": "COMPANY NOT FOUND"}, 404
 
         return result, 200
+
+
+class GetDetailCompany(Resource):
+    def get(self, comp_name):
+        header = request.headers.get("x-wanted-language", "ko")
+        
+        company = Company.query.filter_by(name=comp_name).first()
+        country = Country.query.filter_by(name=header).first()
+
+        if not company or not country:
+            return make_response(
+                jsonify(
+                    {"message" : "Company or Language Not Found"}
+                ), 404
+            )
+
+        company_country = db.session.query(company_countries).filter_by(
+            company_id = company.id, 
+            country_id = country.id
+            ).first().translated_name
+        
+        company_tag_set  = db.session.query(company_tags).filter_by(company_id=company.id)
+        country_tag_list = [db.session.query(country_tags).filter_by(country_id = country.id, tag_id=t.tag_id).first().translated_tag for t in company_tag_set]
+
+        return make_response(
+            jsonify(
+                {
+                    "company_name" : company_country,
+                    "tags" : [
+                        tag
+                    for tag in country_tag_list]
+                }
+            ), 200
+        )
