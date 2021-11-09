980b06b8e1a67f315aedf1d0528b7f4d31b487dd
heejung-gjt <hee_9803@naver.com>
2021-11-09 21:02:41 +0900

    feat: post new company function
    - post method : 회사 생성, 태그 생성, 언어 생성

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask         import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
 
-from company.models import db
-from company.controller import CompanyList, GetDetailCompany
+from company.models     import db
+from company.controller import CompanyList, GetDetailCompany, CompanyCreateView
+from config             import DB_URL
 
-from config import DB_URL
+
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+app.config["SQLALCHEMY_DATABASE_URI"]        = DB_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"]  = True
 
 db.init_app(app)
 db.app = app
@@ -22,6 +23,7 @@ migrate = Migrate(app, db)
 
 api.add_resource(CompanyList, "/search")
 api.add_resource(GetDetailCompany, "/companies/<comp_name>")
+api.add_resource(CompanyCreateView, '/companies')
 
 
 if __name__ == "__main__":
